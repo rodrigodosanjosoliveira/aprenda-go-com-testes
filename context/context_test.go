ac67a0f101393404a3ade615f3dc78a40aa2fb39
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2022-06-06 09:00:57 -0300

    feat: :sparkles: initial commit

---
+++ context/context_test.go
@@ -0,0 +1,60 @@
+package context1
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
+func TestServer(t *testing.T) {
+	data := "ola, mundo"
+
+	t.Run("retorna dados da store", func(t *testing.T) {
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf(`recebi "%s", quero "%s"`, response.Body.String(), data)
+		}
+	})
+
+	t.Run("avisa a store para cancelar o trabalho se a requisição for cancelada", func(t *testing.T) {
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Errorf("store não foi avisada para cancelar")
+		}
+	})
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rodrigodosanjosoliveira/aprenda-com-testes
+
+go 1.18

990b3f4703a07ddfb60860884dc826c348b9fc21
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2022-06-07 20:23:31 -0300

    test: :test_tube: estruturas, metodos e interfaces

---
+++ estruturas-metodos-interfaces/formas_test.go
@@ -0,0 +1,35 @@
+package estruturasmetodosinterfaces
+
+import "testing"
+
+func TestPerimetro(t *testing.T) {
+	retangulo := Retangulo{10.0, 10.0}
+	resultado := Perimetro(retangulo)
+	esperado := 40.0
+
+	if resultado != esperado {
+		t.Errorf("resultado %.2f esperado %.2f", resultado, esperado)
+	}
+}
+
+func TestArea(t *testing.T) {
+	testesArea := []struct {
+		nome    string
+		forma   Forma
+		temArea float64
+	}{
+		{nome: "Retângulo", forma: Retangulo{12, 6}, temArea: 72.0},
+		{nome: "Círculo", forma: Circulo{10}, temArea: 314.1592653589793},
+		{nome: "Triângulo", forma: Triangulo{12, 6}, temArea: 36.0},
+	}
+
+	for _, tt := range testesArea {
+		t.Run(tt.nome, func(t *testing.T) {
+			resultado := tt.forma.Area()
+			if resultado != tt.temArea {
+				t.Errorf("resultado %.2f, esperado %.2f", resultado, tt.temArea)
+			}
+		})
+
+	}
+}
